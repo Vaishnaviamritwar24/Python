0a5f6489b7dc255cecf2be7d8795eec03f7b9c13
“Vaishnaviamritwar24” <Vaishnavi.amritwar@gmail.com>
2023-06-16 17:22:03 +0530

    list concatination

---
+++ lists/List_concatination.py
@@ -0,0 +1,9 @@
+#List concatination and multiplication
+list1 = [1,10,2,20,3,30]
+list2 = ['mumbai','pune','hyderabad','aisha','rahul',50,60,70,80,90,100]
+print("List concatination")
+list3= list1+list2
+print(list3)
+
+print("List multiplication")
+print(list1*3)
