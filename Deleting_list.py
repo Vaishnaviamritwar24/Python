3190c3f24e3b6daee2ae3f8a29545ff5d8ed4de2
“Vaishnaviamritwar24” <Vaishnavi.amritwar@gmail.com>
2023-06-16 17:04:33 +0530

    Deleting_list

---
+++ Deleting_list.py
@@ -0,0 +1,11 @@
+list1 = [1,10,2,20,3,30]
+list2 = ['mumbai','pune','hyderabad','aisha','rahul',50,60,70,80,90,100]
+#deleting the list
+#remove
+print("The removed element is:")
+print(list1)
+list1.remove(30)
+#pop and clear
+print(list2)
+list2.pop()
+list2.clear()
