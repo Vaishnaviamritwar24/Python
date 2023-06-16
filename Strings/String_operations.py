e5c356c0669ad501f435288db021e838e91f94f1
“Vaishnaviamritwar24” <Vaishnavi.amritwar@gmail.com>
2023-06-16 23:28:05 +0530

    String Operations

---
+++ Strings/String_operations.py
@@ -0,0 +1,36 @@
+"""
+STRINGS AND THEIR OPERATIONS
+"""
+
+s1=" Hi I am Vaishnavi"
+s2="Amritwar"
+
+print("Indexing")
+print(s2[2:7])
+
+length = len(s1)
+print("The length of s1",length)
+
+lowercase = s1.lower()
+print("String s1 in lowercase is:",lowercase)
+
+uppercase = s1.upper()
+print("String s1 in uppercase is:",uppercase)
+
+strip = s1.strip()
+print("String s2 strriped is:",strip)
+
+words = s1.split(", ")
+print("The split string is:",words)
+
+string = ", ".join(s1)
+print(string)
+
+replaced = s1.replace("Hi", "Hello")
+print("The replaced string is:",replaced)
+
+
+
+
+
+
