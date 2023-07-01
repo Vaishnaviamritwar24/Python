ff6bfe7b2daf6b341623f0dd167f2b0a062eea26
“Vaishnaviamritwar24” <Vaishnavi.amritwar@gmail.com>
2023-07-01 22:21:02 +0530

    functions

---
+++ Functions/Nested_function.py
@@ -0,0 +1,36 @@
+# Nested function: When we define one function inside another function
+
+#1
+def disp():
+    def show():
+        print("Show Function")
+    print("Display Function")
+    show()
+disp()
+
+#2
+def sample(x,y,z):  #x=10 y=20 z=30
+    print("x={} y={} and z={}".format(x,y,z))
+
+    def innerfunction(a,b): #a=20 b=30
+         print("a={} b={} ".format(a,b))
+         def innermost(k): #k=30
+             print("k={} ".format(k))
+         innermost(b)    
+
+    innerfunction(y,z)
+    
+
+a=10
+b=20
+c=30
+sample(a,b,c)
+
+#3
+def disp(st):
+    def show():
+        return "Show Function"
+    result=show() + st+ " Display Function"
+    return result
+
+print(disp(" Display all"))
---
+++ Functions/lambda_functions.py
@@ -0,0 +1,11 @@
+#Lambda functions
+
+f=lambda x,y : x if x>y else y
+print(f(10,20))
+
+f=lambda x: x*x
+print(f(4))
+
+s=lambda n:"is even" if n%2==0 else "is odd"
+print(s(5))
+print(s(8))
