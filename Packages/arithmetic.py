3ef9208ed33b42be173b6157d63a80a5b34188e5
“Vaishnaviamritwar24” <Vaishnavi.amritwar@gmail.com>
2023-07-06 23:20:45 +0530

    packages

---
+++ Packages/arithmetic.py
@@ -0,0 +1,27 @@
+#Arithmetic
+
+#Addition 
+def add(a,b):
+    print("sum is:",a+b)
+
+#Subtraction
+def sub(a,b):
+    print("diff is:",a-b)
+
+#Multiplication 
+def mul(a,b):
+    print("Product is:",a*b)
+
+#Divison 
+def div(a,b):
+    print("div is:",a/b)
+
+#Floor divison 
+def floordiv(a,b):
+    print("floordiv is:",a//b)
+
+#Remainder (Modulo)
+def rem(a,b):
+    print("remainder is:",a%b)
+
+    
